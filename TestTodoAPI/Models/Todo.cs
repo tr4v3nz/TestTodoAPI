48e24933641ad66651ee31f73b7bab0f7c17a06f
tr4v3nz <tr4v3nztantra@gmail.com>
2019-10-29 22:39:49 +0700

    Add files via upload

---
+++ TestTodoAPI/Models/Todo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTodoAPI.Models
+{
+    public class Todo
+    {
+        public int TodoId { get; set; }
+        public string Title { get; set; }
+        public string Detail { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public int PrecentageProgress { get; set; }
+        public Boolean IsDone { get; set; }
+
+        public Todo todo { get; set; }
+    }
+
+    public class TodoDetail
+    {
+        public int TodoDetailId { get; set; }
+        public string Detail { get; set; }
+        public Boolean IsDone { get; set; }
+    }
+}
